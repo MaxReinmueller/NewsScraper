286582c9ae6e9a506e32065c16f1c1732effb084
Max Reinmueller <maxreinmueller@gmail.com>
2019-10-23 12:01:48 -0700

    created note schema

---
+++ models/Note.js
@@ -0,0 +1,21 @@
+// require the dependencies
+var mongoose = require('mongoose');
+
+// create a schema
+var Schema = mongoose.Schema;
+
+// create a new schema
+var noteSchema = new Schema({
+    // the associated article we want to attach the note to
+    _headlineId: {
+        type: Schema.Types.ObjectId,
+        ref: "Headline"
+    },
+    date: String,
+    noteText: String
+});
+
+// export out the schema
+var Note = mongoose.model("Note", noteSchema);
+
+module.exports = Note;
