995754548c9bcc4f1f0ef906e5b64e1ae7d15a47
Max Reinmueller <maxreinmueller@gmail.com>
2019-10-23 10:53:27 -0700

    handlebars template set up. Set up mongoose

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // require dependencies
 var express = require('express');
+var mongoose = require('mongoose');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 
@@ -28,6 +29,18 @@ app.use(bodyParser.urlencoded({
 
 // make requests go through middleware
 app.use(router);
+
+// Use the database and store it in a variable
+var db = process.env.MONGODB_URI || "mongodb://localhost/newsScraperNews"
+
+// connect mongoose to the database
+mongoose.connect(db, function(err){
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("mongoose connection successful");
+    }
+})
 
 // turn on server
 app.listen(PORT, function () {
